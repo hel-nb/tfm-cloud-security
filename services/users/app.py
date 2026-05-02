4eaabcac0592e28d2e0bdb5ea3f7a8f38b9d4a30
hel-nb <hn.bh@hotmail.com>
2026-05-02 15:04:56 -0500

    Update app.py

--- services/users/app.py
+++ services/users/app.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
-        "statusCode": 500,
-        "body": "Users service ERROR"
+        "statusCode": 200,
+        "body": "Users service DEV"
     }
