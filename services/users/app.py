377380a33e2fa7135d2dd88ab705888ff4b67bc7
hel-nb <hn.bh@hotmail.com>
2026-05-02 15:50:10 -0500

    Update app.py

--- services/users/app.py
+++ services/users/app.py
@@ -1,4 +1,10 @@
 def lambda_handler(event, context):
+    if event.get("fail"):
+        return {
+            "statusCode": 500,
+            "body": "Forced error"
+        }
+        
     return {
         "statusCode": 200,
         "body": "Users service DEV"
